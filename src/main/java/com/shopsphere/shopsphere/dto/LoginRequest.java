f4c4690ff92ec00192cd73b3ae97923ec83b73d8
shubhamhalvadia <68186297+shubhamhalvadia@users.noreply.github.com>
2025-05-26 18:41:28 -0400

    Added authController with jwt token authentication

--- src/main/java/com/shopsphere/shopsphere/dto/LoginRequest.java
+++ src/main/java/com/shopsphere/shopsphere/dto/LoginRequest.java
@@ -1,4 +1,9 @@
 package com.shopsphere.shopsphere.dto;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+    private String email;
+    private String password;
 }
--- src/main/java/com/shopsphere/shopsphere/dto/RegisterRequest.java
+++ src/main/java/com/shopsphere/shopsphere/dto/RegisterRequest.java
@@ -1,4 +1,10 @@
 package com.shopsphere.shopsphere.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
+    private String email;
+    private String password;
+    private String role; // Optional: "USER" or "ADMIN"
 }
